c3410b9b6626d75c46009deaa91e038be9d18d77
Justin Im <20imj68@myahs.org>
2019-09-26 17:39:35 -0700

    Added framework for Kevin to program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleOP.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "TestOPMode", group = "jerW" )
+public class teleOP extends OpMode {
+    //TODO: Create instance variable for drivetrain
+
+    @Override
+    public void init() {
+        //TODO: Initialize Drivetrain Object
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void loop() {
+        //TODO: Controls
+
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
