800fbaa93f15f5589b5608c003c41c78b3cb00fa
Justin Im <20imj68@myahs.org>
2019-10-27 01:12:35 -0700

    Package statement refactor changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.prototypes;
+package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleOP.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.prototypes.ClawArm;
-import org.firstinspires.ftc.teamcode.prototypes.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.ClawArm;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 
 
