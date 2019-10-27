abfc920cd74eddeaca48793519fac2c611291524
Justin Im <20imj68@myahs.org>
2019-10-27 01:01:35 -0700

    bruh moment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoA.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode.auto;
 
-public class AutoA {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+
+import java.sql.Driver;
+
+@Autonomous(name="uwu", group = "pepega")
+public class AutoA extends LinearOpMode {
+    public Drivetrain robot;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new Drivetrain(hardwareMap.dcMotor.get("topRightMotor"), hardwareMap.dcMotor.get("bottomRightMotor"), hardwareMap.dcMotor.get("topLeftMotor"), hardwareMap.dcMotor.get("bottomLeftMotor"), true);
+        waitForStart();
+        robot.drive(10, 1);
+    }
 }
